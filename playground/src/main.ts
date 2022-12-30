287cec8c5316a52eaf8291e16be6266c1b819338
Akos Stegner <a.stegner@mito.hu>
2022-12-30 23:06:32 +0100

    Use plugin installitaion for playground

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+import { ShokaGallery } from 'shoka-gallery'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(ShokaGallery)
+app.mount('#app')
