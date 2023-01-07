cddb2a760c77d903a392803121ca056375695dfe
Akos Stegner <a.stegner@mito.hu>
2023-01-07 21:55:32 +0100

    Develop tests

--- tests/components.spec.ts
+++ tests/components.spec.ts
@@ -1,5 +1,5 @@
 import { defineComponent, nextTick, ref } from 'vue'
-import { describe, it, vi, expect } from 'vitest'
+import { describe, it, vi, expect, beforeEach } from 'vitest'
 import { mount } from '@vue/test-utils'
 import { Gallery, GalleryItem, GalleryPanel, GalleryCaption } from '../src'
 
@@ -53,7 +53,6 @@ describe('Gallery', () => {
       `,
     })
     const wrapper = mount(testComponent)
-    // await nextTick()
 
     const GalleryPanelComp = wrapper.getComponent({ name: 'GalleryPanel' })
     expect(GalleryPanelComp.isVisible()).toBe(false)
@@ -61,7 +60,6 @@ describe('Gallery', () => {
     const triggerComponent = wrapper.findComponent({ name: 'GalleryItem' })
     await triggerComponent.trigger('click')
 
-    // console.log(wrapper.html())
     expect(GalleryPanelComp.isVisible()).toBe(true)
     expect(GalleryPanelComp.attributes('role')).toBe('dialog')
     expect(GalleryPanelComp.attributes('tabindex')).toBe('0')
@@ -89,7 +87,6 @@ describe('Gallery', () => {
       `,
     })
     const wrapper = mount(testComponent)
-    // console.log(wrapper.html())
     const GalleryPanelComp = wrapper.getComponent({ name: 'GalleryPanel' })
     expect(GalleryPanelComp.isVisible()).toBe(true)
   })
@@ -112,7 +109,6 @@ describe('Gallery', () => {
       `,
     })
     const wrapper = mount(testComponent)
-    console.log(wrapper.html())
     const GalleryPanelComp = wrapper.getComponent({ name: 'GalleryPanel' })
     expect(GalleryPanelComp.isVisible()).toBe(true)
   })
@@ -138,16 +134,128 @@ describe('Gallery', () => {
     })
     const wrapper = mount(testComponent)
     expect(wrapper.find('#gallaery-panel').exists()).toBe(true)
-    
+
     const button = wrapper.find('button')
     expect(button.isVisible()).toBe(true)
-    
+
     await button.trigger('click')
-    // console.log(wrapper.html())
     expect(wrapper.find('#gallaery-panel').exists()).toBe(false)
   })
 })
 
-// describe('GalleryItem', () => {
-  
-// })
+describe('GalleryItem', () => {
+  beforeEach(async () => {
+    startId = 0
+  })
+
+  it('Should id on Gallery item', async () => {
+    const testComponent = defineComponent({
+      components: {
+        Gallery,
+        GalleryItem,
+      },
+      setup: () => ({ value: ref(true) }),
+      template: `
+      <Gallery v-model="value">
+        <GalleryItem src="img1" title="title-0">A</GalleryItem>
+      </Gallery>
+      `,
+    })
+    const wrapper = mount(testComponent)
+    expect(wrapper.getComponent({ name: 'GalleryItem' }).attributes('id')).toBe('gallery-item-0')
+    expect(wrapper.getComponent({ name: 'GalleryItem' }).attributes('tabindex')).toBe('0')
+  })
+})
+
+describe('KeyboardInteraction', () => {
+  describe('GalleryIten', () => {
+    it('Should show GalleryPanel when press enter', async () => {
+      const testComponent = defineComponent({
+        components: {
+          Gallery,
+          GalleryItem,
+          GalleryPanel,
+          GalleryCaption,
+        },
+        template: `
+        <Gallery>
+          <GalleryPanel></GalleryPanel>
+          <GalleryItem src="img1" title="title-0">A</GalleryItem>
+        </Gallery>
+        `,
+      })
+      const wrapper = mount(testComponent)
+      expect(wrapper.getComponent({ name: 'GalleryPanel' }).isVisible()).toBe(false)
+      await wrapper.find('#gallery-item-1').trigger('keydown', { key: 'Enter' })
+
+      expect(wrapper.getComponent({ name: 'GalleryPanel' }).isVisible()).toBe(true)
+    })
+  })
+
+  it('Should close GalleryPanel by press Esc', async () => {
+    const testComponent = defineComponent({
+      components: {
+        Gallery,
+        GalleryItem,
+        GalleryPanel,
+        GalleryCaption,
+      },
+      setup: () => ({ value: ref(true) }),
+      template: `
+      <Gallery v-model="value" v-slot:default="{ close }">
+        <GalleryPanel id="gallaery-panel">
+          <button @click="close()"></button>
+        </GalleryPanel>
+        <GalleryItem src="img1" title="title-0">A</GalleryItem>
+        <GalleryItem src="img2" title="title-1">B</GalleryItem>
+      </Gallery>
+      `,
+    })
+    const wrapper = mount(testComponent)
+    const GalleryPanelComp = wrapper.getComponent({ name: 'GalleryPanel' })
+    expect(wrapper.find('#gallaery-panel').exists()).toBe(true)
+
+    await GalleryPanelComp.trigger('keydown', { key: 'Escape' })
+    // console.log(wrapper.html())
+    expect(wrapper.find('#gallaery-panel').exists()).toBe(false)
+  })
+
+  it('Should go the next Item if press arrow up/right, and previous down/left', async () => {
+    const testComponent = defineComponent({
+      components: {
+        Gallery,
+        GalleryItem,
+        GalleryPanel,
+        GalleryCaption,
+      },
+      setup: () => ({ value: ref(true) }),
+      template: `
+      <Gallery v-model="value" v-slot:default="{ close }">
+        <GalleryPanel id="gallaery-panel" v-slot:default="{ currentItem, next }">
+          <div id="current">{{ currentItem?.src }}</div>
+          <button @click="next()"></button>
+        </GalleryPanel>
+        <GalleryItem src="img1" title="title-0">A</GalleryItem>
+        <GalleryItem src="img2" title="title-1">B</GalleryItem>
+        <GalleryItem src="img3" title="title-2">B</GalleryItem>
+      </Gallery>
+      `,
+    })
+    const wrapper = mount(testComponent)
+    const GalleryPanelComp = wrapper.getComponent({ name: 'GalleryPanel' })
+    expect(wrapper.find('#gallaery-panel').exists()).toBe(true)
+
+    await GalleryPanelComp.trigger('keydown', { key: 'ArrowRight' })
+    expect(wrapper.find('#current').text()).toBe('img2')
+    // console.log(wrapper.html())
+
+    await GalleryPanelComp.trigger('keydown', { key: 'ArrowUp' })
+    expect(wrapper.find('#current').text()).toBe('img3')
+
+    await GalleryPanelComp.trigger('keydown', { key: 'ArrowDown' })
+    expect(wrapper.find('#current').text()).toBe('img2')
+
+    await GalleryPanelComp.trigger('keydown', { key: 'ArrowLeft' })
+    expect(wrapper.find('#current').text()).toBe('img1')
+  })
+})
